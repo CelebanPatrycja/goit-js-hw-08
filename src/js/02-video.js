b0cad81d77ffea01ce61417dfbcd89572fafc742
CelebanPatrycja <114691633+CelebanPatrycja@users.noreply.github.com>
2023-07-03 17:41:09 +0200

    update feedback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,3 +2,17 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player');
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  }, 1000)
+);
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash/throttle';
 
+const form = document.querySelector('.feedback-form');
+
+function saveMsg(event) {
+  const dataInput = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataInput));
+}
+
+form.addEventListener('input', throttle(saveMsg, 500));
+
+if (localStorage.getItem('feedback-form-state') !== null) {
+  const formElement = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  form.email.value = formElement.email || '';
+  form.message.value = formElement.message || '';
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const dataInput = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+
+  console.log(dataInput);
+
+  localStorage.removeItem('feedback-form-state');
+  form.email.value = '';
+  form.message.value = '';
+});
